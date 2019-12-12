4e06f9c123f5a1d209c99e3d597e4ddff85d57c7
TsuMakoto <tsukamoto@fusic.co.jp>
2019-12-12 23:38:26 +0900

    Add first article

--- build.sh
+++ build.sh
@@ -1,1 +1,3 @@
+cat index.md > README.html
 `which docker` run --rm --mount type=bind,src=`pwd`,dst=/home/jovyan jupyter/datascience-notebook jupyter nbconvert --to html begginer/mnist.ipynb
+mv begginer/mnist.html begginer/index.html
