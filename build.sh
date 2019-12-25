b555bbd19fcbba5ee1dee7d4f49c7c51d4395333
TsuMakoto <tsukamoto@fusic.co.jp>
2019-12-26 00:11:50 +0900

    bugginer/2 追加

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 cat index.md > README.md
-`which docker` run --rm --mount type=bind,src=`pwd`,dst=/home/jovyan jupyter/datascience-notebook jupyter nbconvert --to html begginer/mnist.ipynb
-mv begginer/mnist.html begginer/index.html
+`which docker` run --rm --mount type=bind,src=`pwd`,dst=/home/jovyan jupyter/datascience-notebook jupyter nbconvert --to html begginer/1/mnist.ipynb
+`which docker` run --rm --mount type=bind,src=`pwd`,dst=/home/jovyan jupyter/datascience-notebook jupyter nbconvert --to html begginer/2/mnist.ipynb
+mv begginer/1/mnist.html begginer/1/index.html
+mv begginer/2/mnist.html begginer/2/index.html
