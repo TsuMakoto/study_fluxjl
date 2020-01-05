8fb80b6510edbeabac55197c25781f42b8be9d63
TsuMakoto <tsukamoto@fusic.co.jp>
2020-01-06 00:38:40 +0900

    markdownにしてみる

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 cat index.md > README.md
 
 for f (begginer/**/*.ipynb) {
-  `which docker` run --rm --mount type=bind,src=`pwd`,dst=/home/jovyan jupyter/datascience-notebook jupyter nbconvert --to html $f
+  `which docker` run --rm --mount type=bind,src=`pwd`,dst=/home/jovyan jupyter/datascience-notebook jupyter nbconvert --to markdown $f
   mv "$f:r.html" "$f:h/index.html"
 }
+
+`which docker` run --rm --mount type=bind,src=`pwd`,dst=/home/jovyan jupyter/datascience-notebook jupyter nbconvert --to markdown begginer/1/mnist.ipynb
