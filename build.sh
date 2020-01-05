fb0fe47c67e1f17ea3cc91867f3fd366c96e0d3d
TsuMakoto <tsukamoto@fusic.co.jp>
2020-01-06 00:19:52 +0900

    test commit

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 cat index.md > README.md
-`which docker` run --rm --mount type=bind,src=`pwd`,dst=/home/jovyan jupyter/datascience-notebook jupyter nbconvert --to html begginer/1/mnist.ipynb
-`which docker` run --rm --mount type=bind,src=`pwd`,dst=/home/jovyan jupyter/datascience-notebook jupyter nbconvert --to html begginer/2/mnist.ipynb
-mv begginer/1/mnist.html begginer/1/index.html
-mv begginer/2/mnist.html begginer/2/index.html
+
+for f (begginer/**/*.ipynb) {
+  `which docker` run --rm --mount type=bind,src=`pwd`,dst=/home/jovyan jupyter/datascience-notebook jupyter nbconvert --to html $f
+  mv "$f:r.html" "$f:h/index.html"
+}
--- index.md
+++ index.md
@@ -1,5 +1,9 @@
 # Fluxjl勉強用
 機械学習用ライブラリFlux.jlを勉強していきます。
 
-- [MNITSをもって、Flux.jlを入門する](https://tsumakoto.github.io/study_fluxjl/begginer/1/index.html)
+## 入門編
+- [MNITSをもって、Flux.jlを入門する](https://nbviewer.jupyter.org/github/TsuMakoto/study_fluxjl/blob/master/begginer/1/mnist.ipynb)
 - [MNITSをもって、Flux.jlを入門する(2)](https://tsumakoto.github.io/study_fluxjl/begginer/2/index.html)
+
+## 強化学習
+- [ε-greedy法を学ぶ](https://tsumakoto.github.io/study_fluxjl/reinforcement-learning/1/index.html)
